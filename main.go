9e203ba525998ba472983b995dd8a271c1fa0cc2
Joe <joe@complex64.com>
2023-03-17 15:55:55 +0100

    fix: rename name -> pattern

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ import (
 
 func main() {
 	var (
-		owner, repo, name, token string
+		owner, repo, pattern, token string
 	)
 	flag.StringVar(&owner, "owner", "", "")
 	flag.StringVar(&repo, "repo", "", "")
-	flag.StringVar(&name, "name", "", "")
+	flag.StringVar(&pattern, "pattern", "", "")
 	flag.StringVar(&token, "token", "", "")
 	flag.Parse()
 
@@ -34,7 +34,7 @@ func main() {
 		}
 
 		for _, rel := range rels {
-			if strings.HasPrefix(rel.GetTagName(), name) && !rel.GetDraft() {
+			if strings.HasPrefix(rel.GetTagName(), pattern) && !rel.GetDraft() {
 				candidates = append(candidates, rel)
 			}
 		}
@@ -58,17 +58,17 @@ func main() {
 	}
 
 	if mostRecent == nil {
-		_, _ = fmt.Fprintf(os.Stderr, "no release matching tag prefix `%s` was found\n", name)
+		_, _ = fmt.Fprintf(os.Stderr, "no release matching tag prefix `%s` was found\n", pattern)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Downloading most recent release of `%s` at tag `%s`...\n", name, mostRecent.GetTagName())
+	fmt.Printf("Downloading most recent release of `%s` at tag `%s`...\n", pattern, mostRecent.GetTagName())
 
 	cmd := exec.Command(
 		"gh", "release", "download",
 		mostRecent.GetTagName(),
 		"--repo", owner+"/"+repo,
-		"--pattern", name,
+		"--pattern", pattern,
 		"--clobber",
 	)
 	cmd.Stderr = os.Stderr
@@ -81,7 +81,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := os.Chmod(name, 0755); err != nil {
+	if err := os.Chmod(pattern, 0755); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "chmod: %s\n", err)
 		os.Exit(1)
 	}
